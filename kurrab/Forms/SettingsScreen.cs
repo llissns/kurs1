e0c59882cddb68be3f0d3431cfaffb3d2b930c13
Stepan Solovyev <ssolovyev@phobos.pro>
2024-03-22 16:44:12 +0300

    Update SettingsScreen.cs

--- kurrab/Forms/SettingsScreen.cs
+++ kurrab/Forms/SettingsScreen.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace kurrab.Formsssssssssssssssssssssss
+namespace kurrab.Forms
 {
     public partial class SettingsScreen : Form
     {
