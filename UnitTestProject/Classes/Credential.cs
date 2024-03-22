57de5c18c68e1198dd51e7bd4cb946ae01248847
ssolovyev <ssolovyev@phobos.pro>
2024-03-22 19:42:18 +0300

    credentialTest fullfiled

--- UnitTestProject/Classes/Credential.cs
+++ UnitTestProject/Classes/Credential.cs
@@ -20,15 +20,29 @@ namespace kurrabUnitTests.Classes
 
             // Arrange: creating credential list
             List < Credential >  credlist = new List<Credential >();
-            credlist.Add(new Credential("admin", "pass"));
-            credlist.Add(new Credential("user", "pass"));
+            credlist.Add(new Credential("admin", "123123"));
+            credlist.Add(new Credential("user", "234234"));
 
 
             // Act: searching for specified credential at list
-            bool result = Credential.searchForCredential(credlist, new Credential("login", "password"));
+            // false cases
+            bool result1 = Credential.searchForCredential(credlist, new Credential("login", "password"));
+            bool result2 = Credential.searchForCredential(credlist, new Credential("admin", "wrong_password"));
+            bool result3 = Credential.searchForCredential(credlist, new Credential("user", "wrong_password"));
+
+            // true cases:
+            bool result4 = Credential.searchForCredential(credlist, new Credential("admin", "123123"));
+            bool result5 = Credential.searchForCredential(credlist, new Credential("user", "234234"));
 
             // Assert: making sure result is true
-            Assert.IsTrue(result);
+            // checking false cases results
+            Assert.IsFalse(result1);
+            Assert.IsFalse(result2);
+            Assert.IsFalse(result3);
+
+            // checking true cases results
+            Assert.IsTrue(result4);
+            Assert.IsTrue(result5);
         }
     }
 }
