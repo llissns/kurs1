1ec15d66ef01137711ccf0608e20a99d0009128a
StepanSolovyev <ssolovyev@phobos.pro>
2024-04-19 20:46:20 +0300

    password field hiden

--- kurrab/Forms/Authentication.Designer.cs
+++ kurrab/Forms/Authentication.Designer.cs
@@ -50,10 +50,10 @@
             this.button1.BackColor = System.Drawing.Color.Ivory;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Times New Roman", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(61, 231);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Location = new System.Drawing.Point(46, 188);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(113, 28);
+            this.button1.Size = new System.Drawing.Size(85, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "ВОЙТИ";
             this.button1.UseVisualStyleBackColor = false;
@@ -62,43 +62,47 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(54, 90);
+            this.label1.Location = new System.Drawing.Point(40, 73);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(45, 16);
+            this.label1.Size = new System.Drawing.Size(36, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "логин";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(49, 142);
+            this.label2.Location = new System.Drawing.Point(37, 115);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(54, 16);
+            this.label2.Size = new System.Drawing.Size(43, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "пароль";
             // 
             // login
             // 
-            this.login.Location = new System.Drawing.Point(111, 84);
-            this.login.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.login.Location = new System.Drawing.Point(83, 68);
+            this.login.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.login.Name = "login";
-            this.login.Size = new System.Drawing.Size(100, 22);
+            this.login.Size = new System.Drawing.Size(76, 20);
             this.login.TabIndex = 3;
             // 
             // password
             // 
-            this.password.Location = new System.Drawing.Point(111, 139);
-            this.password.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.password.Location = new System.Drawing.Point(83, 113);
+            this.password.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.password.Name = "password";
-            this.password.Size = new System.Drawing.Size(100, 22);
+            this.password.PasswordChar = '*';
+            this.password.Size = new System.Drawing.Size(76, 20);
             this.password.TabIndex = 4;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(36, 193);
+            this.textBox1.Location = new System.Drawing.Point(27, 157);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(175, 22);
+            this.textBox1.Size = new System.Drawing.Size(132, 20);
             this.textBox1.TabIndex = 6;
             // 
             // label3
@@ -107,8 +111,9 @@
             this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Ivory;
             this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(239, 73);
+            this.label3.Size = new System.Drawing.Size(179, 59);
             this.label3.TabIndex = 5;
             this.label3.Text = "Авторизация";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -127,16 +132,18 @@
             this.panel1.Controls.Add(this.label2);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(239, 271);
+            this.panel1.Size = new System.Drawing.Size(179, 220);
             this.panel1.TabIndex = 7;
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = global::kurrab.Properties.Resources._326657_https_lock_secure_icon;
-            this.pictureBox2.Location = new System.Drawing.Point(12, 132);
+            this.pictureBox2.Location = new System.Drawing.Point(9, 107);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(29, 29);
+            this.pictureBox2.Size = new System.Drawing.Size(22, 24);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 9;
             this.pictureBox2.TabStop = false;
@@ -144,9 +151,10 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::kurrab.Properties.Resources._3289576_user_icon1;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 79);
+            this.pictureBox1.Location = new System.Drawing.Point(9, 64);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(29, 29);
+            this.pictureBox1.Size = new System.Drawing.Size(22, 24);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 8;
             this.pictureBox1.TabStop = false;
@@ -157,17 +165,18 @@
             this.panel2.Controls.Add(this.label3);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(239, 73);
+            this.panel2.Size = new System.Drawing.Size(179, 59);
             this.panel2.TabIndex = 7;
             // 
             // Authentication
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(239, 270);
+            this.ClientSize = new System.Drawing.Size(179, 219);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.Name = "Authentication";
             this.RightToLeft = System.Windows.Forms.RightToLeft.No;
